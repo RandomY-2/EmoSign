f30e2ecef8a62a71db5c30e7c601e0451010b9d7
Jiahe Yan <yjh020711@ucla.edu>
2021-02-07 09:51:00 +0800

    added sentiment analysis

--- emosign/src/component/signDetection/detection.js
+++ emosign/src/component/signDetection/detection.js
@@ -1,30 +1,81 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
 import * as tf from '@tensorflow/tfjs';
-import {CLASS} from './HelperFunc/mapLabel';
-import {Container, Row, Col} from 'react-bootstrap';
+import * as use from '@tensorflow-models/universal-sentence-encoder';
+import { CLASS } from './HelperFunc/mapLabel';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import './detect.css';
+import posEmo from './img/positive-emoji.png';
+import neuEmo from './img/neutral-emoji.png';
 
-// teachable machine URL: https://teachablemachine.withgoogle.com/models/4virVm-gp/
+const positive = [
+    'HOW ARE YOU?', 'THANK YOU SO MUCH', 'TODAY IS A NICE DAY',
+    'I LIKE PLAYING TENNIS', 'I LIKE YOUR PHONE', 'HAHAHA'
+];
+
+const negative = [
+    'SAD', 'UNHAPPY', 'DEPREESSED', 'ANGRY', 'NO', 'HATE'
+]
+
+const sentences = [
+    // positive
+    'HOW ARE YOU?', 'THANK YOU SO MUCH', 'TODAY IS A NICE DAY',
+    'I LIKE PLAYING TENNIS', 'I LIKE YOUR PHONE', 'HAHAHA',
+
+    // negative
+    'SAD', 'UNHAPPY', 'DEPREESSED', 'ANGRY', 'NO', 'HATE'
+];
 
 function Detection() {
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
     const [text, setText] = useState([]);
-    const [counter, setCounter] = useState(0);
-
-    //const URL = 'https://teachablemachine.withgoogle.com/models/4virVm-gp/';
+    const [emo_model, setModel] = useState(null);
+    const [emotion, setEmotion] = useState('');
 
     const loadModel = async () => {
-        const model = await tf.loadLayersModel('https://cloud-object-storage-7g-cos-standard-40b.s3.us-east.cloud-object-storage.appdomain.cloud/model.json')
+        const sign_model = await tf.loadLayersModel('https://cloud-object-storage-7g-cos-standard-40b.s3.us-east.cloud-object-storage.appdomain.cloud/model.json');
+        const sentence_model = await use.load();
+        setModel(sentence_model);
 
         setInterval(() => {
-            detect(model);
+            detect(sign_model);
         }, 2000);
+    };
+
+    const emo_predict = async(input) => {
+        sentences.push(input);
+        const embeddings = await emo_model.embed(sentences);
+    
+        let score = -1;
+        let index = -1;
+        for (let i = 0; i < sentences.length-1; i++) {
+            const sentenceI = tf.slice(embeddings, [i, 0], [1]);
+            const sentenceJ = tf.slice(embeddings, [sentences.length-1, 0], [1]);
+            const sentenceITranspose = false;
+            const sentenceJTransepose = true;
+            const tempScore = tf.matMul(sentenceI, sentenceJ, sentenceITranspose, sentenceJTransepose).dataSync()[0];
+            console.log(tempScore + " " + sentences[i]);
+            index = score > tempScore ? index : i;
+            score = score > tempScore ? score : tempScore;
+        }
+    
+        sentences.pop();
+
+        if (score < 0.4) {
+            console.log('neutral');
+            setEmotion('neutral');
+        } else if (positive.includes(sentences[index])) {
+            console.log('positive'); 
+            setEmotion('positive');
+        } else {
+            console.log('neutral');
+            setEmotion('neutral');
+        }
     }
 
-    const detect = async (model) => {
+    const detect = async (sign_model) => {
         if (
             typeof webcamRef.current !== "undefined" &&
             webcamRef.current !== null &&
@@ -45,9 +96,8 @@ function Detection() {
             const resized = tf.image.resizeBilinear(img, [64,64]).reverse(1);
             const casted = resized.cast('int32');
             const expanded = resized.expandDims(0);
-            const prediction = await model.predict(expanded.div(255.0)).dataSync();
+            const prediction = await sign_model.predict(expanded.div(255.0)).dataSync();
            
-            // console.log(prediction);
             let index = 0;
             let score = prediction[0];
             for (let i = 0; i < 29; i++) {
@@ -78,6 +128,18 @@ function Detection() {
                 <Row>
                     <Col>
                         <h4>{text}</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button onClick={() => emo_predict(text.toString())} varaint='info'>Get Emotion!</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {emotion === '' ? <div></div> : 
+                         emotion === 'positive' ? <img src={posEmo} /> : 
+                                                  <img src={neuEmo} />}
                     </Col>
                 </Row>
 
