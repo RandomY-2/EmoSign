44025e4f71b31115ceb45a28ecd2d9b6103597d7
Jiahe Yan <yjh020711@ucla.edu>
2021-02-07 21:50:54 +0800

    one more image

--- emosign/src/component/signDetection/detection.js
+++ emosign/src/component/signDetection/detection.js
@@ -30,7 +30,7 @@ const sentences = [
 function Detection() {
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
-    const [text, setText] = useState(['I', ' ', 'L', 'O', 'V', 'E', ' ', 'T', 'H', 'I', 'S']);
+    const [text, setText] = useState([]);
     const [emo_model, setModel] = useState(null);
     const [emotion, setEmotion] = useState('');
     const [loaded, setLoaded] = useState(false);
